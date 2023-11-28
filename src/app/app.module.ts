a1683e6f85a90efde845faf65235c8ef8431d16b
Cosmin <kosmyn1994@gmail.com>
2023-11-28 12:55:00 +0100

    secondo giorno di Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsersComponent } from './components/users/users.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'pagina',
+    component: PaginaComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsersComponent,
+    PaginaComponent,
+    NewUserComponent,
+    HomeComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
